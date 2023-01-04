b77da767fbab09effe56962d97d3525e47733d56
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2023-01-04 10:00:18 +0700

    finished sidebar animation with back button

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // console.log("Hi");
 
 const sideBtn = document.querySelector('.hamburger-menu');
+const sideBack = document.querySelector('#sidebar-back');
+
 let btnOpen = false;
 sideBtn.addEventListener('click', () => {
     if (!btnOpen) {
@@ -8,10 +10,20 @@ sideBtn.addEventListener('click', () => {
         btnOpen = true;
 
         document.querySelector('nav').classList.add('open');
-    } else {
+    } 
+    // else {
+    //     sideBtn.classList.remove('open');
+    //     btnOpen = false;
+        
+    //     document.querySelector('nav').classList.remove('open');
+    // }
+});
+
+sideBack.addEventListener('click', () => {
+    if (btnOpen) {
         sideBtn.classList.remove('open');
         btnOpen = false;
         
         document.querySelector('nav').classList.remove('open');
     }
-});
+})
