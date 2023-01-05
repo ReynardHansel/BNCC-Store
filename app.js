43d1ac03a376db6091f99c4b5c7bab62368ffa5e
itztann <115938605+itztann@users.noreply.github.com>
2023-01-05 18:42:13 +0700

    actual searchbar
    js for searchbar

--- app.js
+++ app.js
@@ -46,3 +46,22 @@ var counter = 1;
           counter = 1;
         }
       }, 5000);
+
+      const search = document.querySelector('#input');
+      const itemsContainer = document.querySelector('.items-grid');
+      
+      search.addEventListener('input', searchEvent);
+      
+      function searchEvent(e){ 
+          const text = e.target.value.toLowerCase();
+          const items = itemsContainer.querySelectorAll('.item-box');
+          Array.from(items).forEach(function(item){
+              const itemList= item.children[1].textContent;
+              console.log(item)
+               if(itemList.toLowerCase().indexOf(text) !=-1){
+                   item.style.display = 'block';
+               } else{
+                   item.style.display = 'none';
+               }
+          })
+      }      
